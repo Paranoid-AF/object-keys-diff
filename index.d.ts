7c71ea68be7b87925946a5f68b9ee1a9ea7d04ce
Paranoid-AF <7659335+Paranoid-AF@users.noreply.github.com>
2020-09-18 13:26:48 +0800

    Update declaration file.

--- index.d.ts
+++ index.d.ts
@@ -1,24 +1,17 @@
 /**
 Compares multiple designated key's value in two objects.
 
-@returns
-{
-  key1: true,
-  key2: false,
-  ...
-}
-
 @example
 ```
-const keyDiff = require('object-keys-diff')
+const keysDiff = require('object-keys-diff')
 let keyArr = ["parent"]
 keyArr.push("properties.title.text")
 // keyArr = ["parent", "properties.title.text"]
 
-keyDiff(obj1, obj2, keyArr)
+keysDiff(obj1, obj2, keyArr)
 ```
 */
 
-declare function keysDiff(object1: object, object2: object, keys: Array<string>): object
+declare function keysDiff(object1: object, object2: object, keys: Array<string> | string): object
 
 export = keysDiff
